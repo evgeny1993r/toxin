34dabe9d444635567928032f13094a9ea753bb00
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-04-10 19:41:18 +0300

    Обновление subscription-text-field.js
    Добавление функции focus/blur

--- src/components/subscription-text-field/subscription-text-field.js
+++ src/components/subscription-text-field/subscription-text-field.js
@@ -1,1 +1,12 @@
 import './subscription-text-field.scss'
+
+$(document).ready(() => {
+    const $subscription_text_field = $('.subscription-text-field')
+    const $subscription_text_field_input = $('.subscription-text-field__input')
+    $subscription_text_field_input.on('focus', () => {
+        $subscription_text_field.addClass('subscription-text-field_active')
+    })
+    $subscription_text_field_input.on('blur', () => {
+        $subscription_text_field.removeClass('subscription-text-field_active')
+    })
+})
