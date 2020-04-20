6395d36a892804c941f260d306b7889c4c60e678
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-04-20 16:38:26 +0300

    Create sign-up
    Верстка и стиализация элемента (pixel perfect)

---
+++ src/components/sign-up/sign-up.js
@@ -0,0 +1,11 @@
+import './sign-up.scss'
+
+import '../text-field/text-field'
+import '../radio-buttons/radio-buttons'
+import { MaskedTextField } from '../masked-text-field/masked-text-field'
+import '../toggle/toggle'
+import '../buttons/buttons'
+
+$(document).ready(() => {
+    MaskedTextField()
+})
