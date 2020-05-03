38578b45a8506a882d5b5ab957415a5571888689
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-05-03 14:20:28 +0300

    Create authorization
    Сборка страницы авторизации. Верстка и стиализация

---
+++ src/pages/authorization/authorization.js
@@ -0,0 +1,13 @@
+import './authorization.scss'
+
+import { Header } from '../../components/header/header'
+import '../../components/sign-in/sign-in'
+import '../../components/footer/footer'
+
+function Authorization () {
+    Header()
+}
+
+export {
+    Authorization
+}
