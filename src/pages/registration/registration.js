f4767a5125efc089596862788a3ec1d430ba054d
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-05-03 15:40:22 +0300

    Create registration
    Сборка страницы регистрации. Верстка и стиализация (rem)

---
+++ src/pages/registration/registration.js
@@ -0,0 +1,14 @@
+import './registration.scss'
+
+import { Header } from '../../components/header/header'
+import { SignUp } from '../../components/sign-up/sign-up'
+import '../../components/footer/footer'
+
+function Registration () {
+    Header()
+    SignUp()
+}
+
+export {
+    Registration
+}
