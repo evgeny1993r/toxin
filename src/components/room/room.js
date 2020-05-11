15dc3b747adde4d033f097e586c7a2d466f31d7a
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-05-11 17:49:22 +0300

    Update room
    Мелкие правки + полная замена функциональности (js)

--- src/components/room/room.js
+++ src/components/room/room.js
@@ -4,16 +4,19 @@ import '../rate-button/rate-button'
 
 function Room () {
 
-    // Объявление переменных
+    // Объявление переменных 
     const $btn_left = $('.js-btn-left')
     const $btn_right = $('.js-btn-right')
-    const $image_items = $('.js-images').children()
-    const $circle_items = $('.js-circles').children()
 
-    let value = 0
+    $btn_right.on('click', (e) => {
+        const $room = $(e.target).parents('.room')
+        const $image_items = $room.find('.js-images').children()
+        const $circle_items = $room.find('.js-circles').children()
 
-    // Функция, обновления элементов circle
-    function updateCircles (value) {
+        let value = Number($image_items.attr('data-translatex'))
+        if(value === -810) return 
+        value += -270
+        
         if(value === 0) {
             $circle_items.removeClass('circle_active')
             $circle_items.eq(0).addClass('circle_active')
@@ -27,20 +30,36 @@ function Room () {
             $circle_items.removeClass('circle_active')
             $circle_items.eq(3).addClass('circle_active')
         }
-    }
 
-    $btn_left.on('click', () => {
-        if(value === 0) return 
-        value += 270
         $image_items.css({'transform': `translateX(${value}px)`})
-        updateCircles(value)
+        $image_items.attr('data-translatex', value)
     })
 
-    $btn_right.on('click', () => {
-        if(value === -810) return 
-        value -= 270
-        $image_items.css({'transform':`translateX(${value}px)`})
-        updateCircles(value)
+    $btn_left.on('click', (e) => {
+        const $room = $(e.target).parents('.room')
+        const $image_items = $room.find('.js-images').children()
+        const $circle_items = $room.find('.js-circles').children()
+
+        let value = Number($image_items.attr('data-translatex'))
+        if(value === 0) return 
+        value += 270
+        
+        if(value === 0) {
+            $circle_items.removeClass('circle_active')
+            $circle_items.eq(0).addClass('circle_active')
+        } else if(value === -270) {
+            $circle_items.removeClass('circle_active')
+            $circle_items.eq(1).addClass('circle_active')
+        } else if(value === -540) {
+            $circle_items.removeClass('circle_active')
+            $circle_items.eq(2).addClass('circle_active')
+        } else if(value === -810) {
+            $circle_items.removeClass('circle_active')
+            $circle_items.eq(3).addClass('circle_active')
+        }
+
+        $image_items.css({'transform': `translateX(${value}px)`})
+        $image_items.attr('data-translatex', value)
     })
 }
 
