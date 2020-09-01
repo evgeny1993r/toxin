8aef0acf47b7af950188ca67215349c3d74ef1ae
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-09-01 11:50:43 +0300

    Updata date-dropdown

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -3,55 +3,77 @@ import 'air-datepicker'
 import 'air-datepicker/dist/css/datepicker.min.css'
 import '../../styles/datepicker.scss'
 
-function DateDropdown (one_date, two_date) {
-    // Объявление переменных 
-    const $entry_input = $('.entry-input')
-    const $check_out_input = $('.check-out-input')
-    const icon = `<i class="icon-expand_more"></i>`
+function DateDropdown (id, isShow, oneDate, twoDate) {
 
-    // Создание datepicker
-    const $datepicker = $('#datepicker-date-dropdown')
+    const $datepicker = $(`#${id}`)
+    const $entry = $datepicker.parent('.js-date-dropdown').find('.js-date-dropdown__entry')
+    const $entryInput = $entry.children('.js-date-dropdown__input')
+    const $checkOut = $datepicker.parent('.js-date-dropdown').find('.js-date-dropdown__check-out')
+    const $checkOutInput = $checkOut.children('.js-date-dropdown__input')
+
     $datepicker.datepicker({
         classes: 'datepicker-date-dropdown',
         navTitles: {
             days: 'MM <i>yyyy</i>'
         },
         nextHtml: `
-            <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg class="datepicker--next-html" width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/>
             </svg>
         `,
         prevHtml: `
-            <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg class="datepicker--prev-html" width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/>
             </svg>
         `,
-        offset: 7,
         clearButton: true,
         range: true,
         minDate: new Date(),
         onSelect(fd, date, inst) {
             if(date.length === 0) {
-                $entry_input.html(`ДД.ММ.ГГГГ ${icon}`)
-                $check_out_input.html(`ДД.ММ.ГГГГ ${icon}`)
+                $entryInput.val(`ДД.ММ.ГГГГ`)
+                $checkOutInput.val(`ДД.ММ.ГГГГ`)
             } else if(date.length === 1) {
-                $entry_input.html(`${fd} ${icon}`)
+                $entryInput.val(`${fd}`)
             } else if(date.length === 2) {
-                $check_out_input.html(fd.split(',')[1] + icon) 
+                $checkOutInput.val(fd.split(',')[1])
             }
         }
-    }).data('datepicker').selectDate([one_date, two_date])
+    }).data('datepicker').selectDate([oneDate, twoDate])
 
-    // Создание и добавление кнопки Применить
-    const button_apply = $('<span>', {
-        class: 'datepicker--button js-datepicker__apply',
-        text: 'Применить',
-        'data-action': 'hide'
-    })
-    const $datepicker_buttons = $('.datepicker-date-dropdown').find('.datepicker--buttons')
-    $datepicker_buttons.append(button_apply)
+    if(!isShow) $datepicker.hide()
     
+    $(document).on('click', (e) => {
+        if(isShow) return 
+        if($(e.target).attr('class') === undefined) {
+            $datepicker.hide()
+        } else {
+            if($(`.${$(e.target).attr('class').split(' ')[0]}`).parents('.date-dropdown').length === 0) {
+                $datepicker.hide()
+            }
+        } 
+    })
+
+    $entry.on('click', () => {
+        $datepicker.show()
+    })
+
+    $checkOut.on('click', () => {
+        $datepicker.show()
+    })
+
+    const $datepickerButtons = $datepicker.find('.datepicker--buttons')
+    $datepickerButtons.append(
+        $('<span>', {
+            class: 'datepicker--button js-datepicker__apply',
+            text: 'Применить',
+            'data-action': 'hide',
+            click: () => $datepicker.hide()
+        })
+    )
+
     return $datepicker.data('datepicker')
+
 }
 
 export {
