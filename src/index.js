387f7c68453b80bd0eeac7bd4e3d12d3fdb850a5
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-09-01 12:12:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import './styles/index.scss'
-import './styles/fonts.scss'
-
-//import { Authorization } from './pages/authorization/authorization'
-//import { Registration } from './pages/registration/registration'
-//import { LandingPage } from './pages/landing-page/landing-page'
-//import { RoomDetails } from './pages/room-details/room-details'
-//import { SearchRoom } from './pages/search-room/search-room'
-
-$(document).ready(() => {
-    //Authorization()
-    //Registration()
-    //LandingPage()
-    //RoomDetails()
-    //SearchRoom()
-}) 
+import './styles/fonts.scss' 
+import './styles/navigation.scss'
