c769cad8bf00e4e03d6dc91ae8464b889eac20e7
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-09-01 12:03:01 +0300

    Updata search-room

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -2,26 +2,35 @@ import './search-room.scss'
 
 import { Header } from '../../components/header/header'
 import { FilterDateDropdown } from '../../components/filter-date-dropdown/filter-date-dropdown'
-import { DropdownGuests } from '../../components/dropdown-guests/dropdown-guests'
-import { RangeSlider } from '../../components/range-slider/range-slider'
+import '../../components/dropdown-guests/dropdown-guests'
+import '../../components/dropdown-guests/dropdown-guests.css'
+import '../../components/range-slider/range-slider'
+import '../../components/range-slider/range-slider.css'
+import '../../components/range-slider/range-slider.scss'
 import '../../components/checkbox-buttons/checkbox-buttons'
 import '../../components/rich-checkbox-buttons/rich-checkbox-buttons'
-import { DropdownNumber } from '../../components/dropdown-number/dropdown-number'
+import '../../components/dropdown-number/dropdown-number'
+import '../../components/dropdown-number/dropdown-number.css'
 import { ExpandableCheckboxList } from '../../components/expandable-checkbox-list/expandable-checkbox-list'
+import { Room } from '../../components/room/room'
 import { Pagination } from '../../components/pagination/pagination'
 import { Footer } from '../../components/footer/footer'
 
-function SearchRoom () {
+$(document).ready(() => {
     Header()
-    FilterDateDropdown(new Date('6 1, 2020'), new Date('6 6, 2020'))
-    DropdownGuests(2, 0, 1)
-    RangeSlider()
-    DropdownNumber(2, 2, 0)
+    FilterDateDropdown('js-filter-date-dropdown', new Date('1 September, 2020'), new Date('4 September, 2020'))
+    $('#dropdown-guests').dropdownGuests([2, 0, 1])
+    $('#range-slider').slider({
+        min_value: 0,
+        max_value: 15000,
+        interval: true,
+        current_value_min: 5000,
+        current_value_max: 10000,
+        $input: $('#js-range-slider-input'),
+        symbol: '₽'
+    })
+    $('#dropdown-number').dropdownNumber([2, 2, 0])
     ExpandableCheckboxList()
-    Pagination()
+    Pagination('room', 'pagination__rooms-container', 'pagination__container', Room)
     Footer()
-}
-
-export {
-    SearchRoom
-}
+})
