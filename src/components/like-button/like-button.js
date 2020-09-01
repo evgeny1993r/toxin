0a3cd18bf25450688b6e9e1f3852bc0c657c372c
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-09-01 11:53:49 +0300

    Updata like-button

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,24 +1,25 @@
 import './like-button.scss'
 
 function LikeButton () {
-    const $like_button = $('.js-like-button')
-    $like_button.on('click', (e) => {
-        const $icon = $(e.currentTarget).find('.js-icon')
-        const $like_button_value = $(e.currentTarget).find('.js-like-button__value')
-        let value = Number($like_button_value.text())
-        console.log(value)
-        if($(e.currentTarget).hasClass('like-button_active')) {
-            $icon.removeClass('icon-like_button_active')
-            $icon.addClass('icon-like_button_inActive')
-            $(e.currentTarget).removeClass('like-button_active')
-            $like_button_value.text(value - 1)
-        } else {
-            $icon.removeClass('icon-like_button_inActive')
-            $icon.addClass('icon-like_button_active')
-            $(e.currentTarget).addClass('like-button_active')
-            $like_button_value.text(value + 1)
-        }
-    })    
+    const $likeButton = $('.js-like-button')
+    $likeButton.each((idx, el) => {
+        $(el).on('click', () => {
+            const $icon = $(el).find('.js-icon')
+            const $likeButtonValue = $(el).find('.js-like-button__value')
+            let value = Number($likeButtonValue.text())
+            if($(el).hasClass('like-button_active')) {
+                $icon.removeClass('icon-like_button_active')
+                $icon.addClass('icon-like_button_inActive')
+                $(el).removeClass('like-button_active')
+                $likeButtonValue.text(value - 1)
+            } else {
+                $icon.removeClass('icon-like_button_inActive')
+                $icon.addClass('icon-like_button_active')
+                $(el).addClass('like-button_active')
+                $likeButtonValue.text(value + 1)
+            }    
+        })
+    })  
 }
 
 export {
