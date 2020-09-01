0a7f58635ded281ed62f2d083431424bcad4395c
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-09-01 11:52:41 +0300

    Updata header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,10 +3,13 @@ import './header.scss'
 import '../buttons/buttons'
 
 function Header () {
-    const $link = $('.nav__link')
-    $link.on('click', (e) => {
-        $link.removeClass('nav__link_active')
-        $(e.currentTarget).addClass('nav__link_active')
+    const $header = $('.js-header')
+    $header.each((idx, el) => {
+        const $nav_link = $(el).find('.js-nav__link')
+        $nav_link.on('click', (e) => {
+            $nav_link.removeClass('nav__link_active')
+            $(e.currentTarget).addClass('nav__link_active')
+        })
     })
 }
 
