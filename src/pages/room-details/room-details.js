166899a8e60edce19141a18cd90d09e71d869bcc
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-09-01 12:02:47 +0300

    Updata room-details

--- src/pages/room-details/room-details.js
+++ src/pages/room-details/room-details.js
@@ -3,27 +3,19 @@ import './room-details.scss'
 import { Header } from '../../components/header/header'
 import '../../components/intelligence/intelligence'
 import '../../components/impression/impression'
-import { Comment } from '../../components/comment/comment'
+import { Comment} from '../../components/comment/comment'
 import '../../components/bullet-list/bullet-list'
 import '../../components/cancel/cancel'
 import { Check } from '../../components/check/check'
 import { Footer } from '../../components/footer/footer'
 
-function RoomDetails () {
-    const prace = 9990
-    const date = {
-        entry: new Date('06 01, 2020'),
-        check_out: new Date('06 06, 2020')
-    }
-    const guests = [2, 1, 0]
-    const discount = 2179
-    const collection = 300
+$(document).ready(() => {
     Header()
     Comment()
-    Check(prace, date, guests, discount, collection)
+    Check(
+        'js-date-dropdown', false, new Date('1 September, 2020'), new Date('5 September, 2020'), 
+        'js-dropdown-guests', [2, 1, 0], 
+        9990, 2179, 300
+    )
     Footer()
-}
-
-export {
-    RoomDetails
-}
+})
